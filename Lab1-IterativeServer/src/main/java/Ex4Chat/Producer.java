63b3fdc8a602204e6781540cc8477c23f4d95716
Andrea Passini <andreapa98@gmail.com>
2022-04-26 17:54:04 +0200

    Producer

--- Lab1-IterativeServer/src/main/java/Ex4Chat/Producer.java
+++ Lab1-IterativeServer/src/main/java/Ex4Chat/Producer.java
@@ -9,22 +9,41 @@ public class Producer implements Runnable {
     private final String id;
     private final Queue queue;
 
-    public Producer(String id, Queue q) { this.id = id; queue = q; }
+    public Producer(String id, Queue q) {
+        this.id = id; queue = q;
+    }
 
     public void run() {
+        // Read from keyboard
+
+        // input stream initialization (from user keyboard)
+        BufferedReader inFromUser =
+                new BufferedReader(new InputStreamReader(System.in));
+
+        String fromKeyboard;
+
         try {
-            String fromKeyboard = produce();
-
-            System.out.println("Prod. " + id + ": inserisco " + fromKeyboard);
-            queue.put(fromKeyboard);
-
+            fromKeyboard = inFromUser.readLine();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
+        while (fromKeyboard != null){
+            try {
+                produce(fromKeyboard);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            System.out.println("Prod. " + id + ": inserisco " + fromKeyboard);
+            queue.put(id + "_|_" + fromKeyboard);
+
+
+        }
+
     }
 
-    public String produce() throws IOException {
-        String msg;
+    public String produce(String msg) throws IOException {
 
         // input stream initialization (from user keyboard)
         BufferedReader inFromUser =
