ca90f8c0cc5efcb21b38348d1e5eb2ff2a7db2ee
Andrea Passini <andreapa98@gmail.com>
2022-04-28 09:58:49 +0200

    Create Class Message for JSON encapsulation

---
+++ Lab1-IterativeServer/src/main/java/Ex4Chat/Message.java
@@ -0,0 +1,11 @@
+package Ex4Chat;
+
+public class Message {
+    private String id;
+    private String msg;
+
+    public Message(String id, String msg) {
+        this.id = id;
+        this.msg = msg;
+    }
+}
