34ac19d1c6d715670ef8256f614ef1bd77b69e8c
Andrea Passini <andreapa98@gmail.com>
2022-04-29 08:11:52 +0200

    Client WriteOnly

--- Lab1-IterativeServer/src/main/java/Ex4Chat2/Client.java
+++ Lab1-IterativeServer/src/main/java/Ex4Chat2/Client.java
@@ -1,9 +1,12 @@
 package Ex4Chat2;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class Client {
-
+    // Client will only writes
     public static void main(String[] args) throws Exception {
         int portNumber;
         String request;
@@ -17,6 +20,29 @@ public class Client {
         Socket clientSocket = new Socket(address, portNumber);
         System.out.println("Connecting");
 
+        // input stream initialization (from user keyboard)
+        BufferedReader inFromUser =
+                new BufferedReader(new InputStreamReader(System.in));
 
+        // output stream towards Server
+        DataOutputStream outToServer =
+                new DataOutputStream(clientSocket.getOutputStream());
+
+        System.out.println("Welcome to Write Only Chat ");
+        System.out.println("Write 0 to exit");
+        request = "1";
+
+        //Cicle
+        while (request.equals("0")){
+            System.out.println("Write something: ");
+            request = inFromUser.readLine();
+
+            if(request != null && !request.equals("0")){
+                //Send message
+                System.out.println("Message Sent ");
+
+                outToServer.writeBytes(request);
+            }
+        }
     }
 }
