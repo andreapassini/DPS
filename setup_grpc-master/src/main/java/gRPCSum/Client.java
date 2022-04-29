46887f8fb2744685bbd4789ec87ee39f0aeba626
Andrea Passini <andreapa98@gmail.com>
2022-04-29 11:29:19 +0200

    Update Client.java

--- setup_grpc-master/src/main/java/gRPCSum/Client.java
+++ setup_grpc-master/src/main/java/gRPCSum/Client.java
@@ -2,19 +2,19 @@ package gRPCSum;
 
 public class Client {
     public static void main(String[] args) {
+
+        // Insert number
+
+        // Insert type of Operation
+
+
         System.out.println("Trying to call greeting synchronous method:\n");
 
         //synchronousCall();
 
         System.out.println("\n...Done!");
-
-        System.out.println("--------------");
-
-        System.out.println("Now calling streamGreeting asynchronous method:\n");
-
-        System.out.println("\n...Done!");
     }
 
-    
+
 
 }
