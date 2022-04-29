0def4c389e2f5a470794d40d66883c56671aa3b9
Andrea Passini <andreapa98@gmail.com>
2022-04-29 08:20:08 +0200

    Update ServerThread.java

--- Lab1-IterativeServer/src/main/java/Ex4Chat2/ServerThread.java
+++ Lab1-IterativeServer/src/main/java/Ex4Chat2/ServerThread.java
@@ -1,4 +1,22 @@
 package Ex4Chat2;
 
-public class ServerThread {
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.net.Socket;
+
+public class ServerThread extends Thread {
+
+    private Socket connectionSocket = null;
+    private BufferedReader inFromClient;
+    private DataOutputStream outToClient;
+    private Queue q;
+
+    public ServerThread(Socket connectionSocket, Queue q) {
+        this.connectionSocket = connectionSocket;
+        this.q = q;
+
+        // Start Producer
+
+        // Start Consumer
+    }
 }
